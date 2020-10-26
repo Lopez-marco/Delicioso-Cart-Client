adb916414b6f0d6195531d6ec882aad8e0c7d13f
lopez-marco <lopez.marcohdez@gmail.com>
2020-10-25 23:05:59 -0400

    first push to heroku

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3001';
         break;
-    case 'delicioscartfront.herokuapp.com':
-        APIURL = 'https://deliciosocartserver.herokuapp.com'
+    case 'https://delicioso-cart-client.herokuapp.com':
+        APIURL = 'https://delicioso-cart-server.herokuapp.com'
 }
 
 export default APIURL
